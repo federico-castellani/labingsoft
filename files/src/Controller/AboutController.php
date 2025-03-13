31ba7072170a78a3da20a7f00d162230c7dbdd43
Riccardo Bastianini <RBastianini@users.noreply.github.com>
2025-03-13 11:26:53 +0100

    Altre dipendenze

--- files/src/Controller/AboutController.php
+++ files/src/Controller/AboutController.php
@@ -4,18 +4,20 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class AboutController
+class AboutController extends AbstractController
 {
     #[Route('/about/{subject}', name: 'about')]
     public function index(?string $subject = null): Response
     {
-        if (null !== $subject) {
-            return new Response("<html><body>Ciao $subject!</body></html>"); // QUESTA COSA NON SI FA MAI!
-        }
-
-        return new Response('<html><body>Questa è la nostra applicazione</body></html>');
+        return $this->render(
+            'about/index.html.twig',
+            [
+                'subject' => $subject,
+            ]
+        );
     }
 }
