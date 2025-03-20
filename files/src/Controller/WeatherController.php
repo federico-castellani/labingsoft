1ca2ae9fb45c71906b9785226a2fdbef9c2e7831
federico-castellani <fedecast.16@gmail.com>
2025-03-20 09:22:22 +0100

    Aggiunta requirments sul route di weather

--- files/src/Controller/WeatherController.php
+++ files/src/Controller/WeatherController.php
@@ -7,10 +7,16 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Requirement\Requirement;
 
 class WeatherController extends AbstractController
 {
-    #[Route('/weather/{state}/{city}', name: 'weather')]
+    #[Route('/weather/{state}/{city}',
+        name: 'weather',
+        requirements: [
+            'state' => '[a-zA-Z]{2}',
+            'city' => Requirement::ASCII_SLUG,
+        ])]
     public function index(?string $state = null, ?string $city = null): Response
     {
         return $this->render('weather/index.html.twig', [
