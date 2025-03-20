ab398ddca1d1e5de9985901d05549cd0bc5d3f8f
federico-castellani <fedecast.16@gmail.com>
2025-03-20 12:13:59 +0100

    Creazione comando location

---
+++ files/src/Command/CreateLocationCommand.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Command;
+
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+#[AsCommand(name: 'app:location:create', description: 'Adds a new location')]
+class CreateLocationCommand extends Command
+{
+    const ARG_LOCATION_NAME = 'location_name';
+    const ARG_STATE = 'state';
+
+    public function configure(): void
+    {
+        $this->addArgument(self::ARG_LOCATION_NAME, InputArgument::REQUIRED, 'The location\'s name.');
+        $this->addArgument(self::ARG_STATE, InputArgument::REQUIRED, 'The location\'s state.');
+    }
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        return Command::SUCCESS;
+    }
+}
